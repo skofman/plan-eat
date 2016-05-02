e377eb56e7740b0f8d1d8250dd95b67c29be0c2a
skofman <shlomo.kofman@gmail.com>
2016-05-02 16:16:01 -0700

    Fixed controllers bug on login.

--- routes/getfoods.js
+++ routes/getfoods.js
@@ -5,31 +5,36 @@ var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://plan:eat@ds021711.mlab.com:21711/planeat';
 
 router.get('/', function(req, res) {
-  mongoClient.connect(url, function(err, db) {
-    if (!err) {
-      var users = db.collection('users');
-      users.find({session: Number(req.cookies.session)}).toArray(function(err, results) {
-        if (!err) {
-          var user = db.collection(results[0].username);
-          user.find(req.query).toArray(function(err, results) {
-            if (!err) {
-              res.json(results).status(200);
-            }
-            else {
-              res.sendStatus(500);
-            }
-            db.close();
-          })
-        }
-        else {
-          res.sendStatus(500);
-        }
-      });
-    }
-    else {
-      res.sendStatus(500);
-    }
-  })
+  if (req.cookies.session) {
+    mongoClient.connect(url, function(err, db) {
+      if (!err) {
+        var users = db.collection('users');
+        users.find({session: Number(req.cookies.session)}).toArray(function(err, results) {
+          if (!err) {
+            var user = db.collection(results[0].username);
+            user.find(req.query).toArray(function(err, results) {
+              if (!err) {
+                res.json(results).status(200);
+              }
+              else {
+                res.sendStatus(500);
+              }
+              db.close();
+            })
+          }
+          else {
+            res.sendStatus(500);
+          }
+        });
+      }
+      else {
+        res.sendStatus(500);
+      }
+    })
+  }
+  else {
+    res.sendStatus(401);
+  }
 })
 
 module.exports = router;
